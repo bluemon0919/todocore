45a95b79e60825dfb0cd70d6af774805fddff588
Kota Aoki <bluemon0919@gmail.com>
2020-05-31 16:21:47 +0900

    serverテスト追加

---
+++ todo/server_test.go
@@ -0,0 +1,80 @@
+package todo
+
+import (
+	"log"
+	"net/http/httptest"
+	"testing"
+	"todotool/entity"
+)
+
+func TestServerGetActive(t *testing.T) {
+	ent := entity.NewEntityMap()
+	srv := NewServer(":8080", ent)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/?kind=active", nil)
+	srv.handler(w, r)
+	resp := w.Result()
+
+	defer resp.Body.Close()
+	ctype := resp.Header.Get("Content-Type")
+	if "application/json" != ctype {
+		log.Fatalf("Content-Type does not match. %s\n", ctype)
+	}
+}
+
+func TestServerGetComplete(t *testing.T) {
+	ent := entity.NewEntityMap()
+	srv := NewServer(":8080", ent)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/?kind=complete", nil)
+	srv.handler(w, r)
+	resp := w.Result()
+
+	defer resp.Body.Close()
+	ctype := resp.Header.Get("Content-Type")
+	if "application/json" != ctype {
+		log.Fatalf("Content-Type does not match. %s\n", ctype)
+	}
+}
+
+func TestServerDelete(t *testing.T) {
+	ent := entity.NewEntityMap()
+	srv := NewServer(":8080", ent)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("DELETE", "/", nil)
+	srv.handler(w, r)
+	resp := w.Result()
+
+	defer resp.Body.Close()
+
+	// expect
+}
+
+func TestServerAdd(t *testing.T) {
+	ent := entity.NewEntityMap()
+	srv := NewServer(":8080", ent)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("PUT", "/?kind=add", nil)
+	srv.handler(w, r)
+	resp := w.Result()
+
+	defer resp.Body.Close()
+	// expect
+}
+
+func TestServerUpdate(t *testing.T) {
+	ent := entity.NewEntityMap()
+	srv := NewServer(":8080", ent)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("PUT", "/?kind=update", nil)
+	srv.handler(w, r)
+	resp := w.Result()
+
+	defer resp.Body.Close()
+	// expect
+}
