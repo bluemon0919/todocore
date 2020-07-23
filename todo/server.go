ad031a619c3dd064eb2b3e685164237d60796b29
Kota Aoki <bluemon0919@gmail.com>
2020-07-23 17:23:08 +0900

    不要コードを削除

--- todo/server.go
+++ todo/server.go
@@ -1,8 +1,6 @@
 package todo
 
 import (
-	"encoding/json"
-	"fmt"
 	"html/template"
 	"net/http"
 	"sort"
@@ -88,112 +86,9 @@ func NewServer(addr string, ent entity.Entity) *Server {
 
 // StartService starts http server.
 func (srv *Server) StartService() error {
-	//http.HandleFunc("/", srv.handler)
 	http.HandleFunc("/list", srv.listHandler)
 	http.HandleFunc("/post", srv.postHandler)
 	return http.ListenAndServe(srv.addr, nil)
-}
-
-func (srv *Server) handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		srv.get(w, r)
-	case http.MethodDelete:
-		srv.delete(w, r)
-	case http.MethodPut:
-		srv.put(w, r)
-	default:
-		err := fmt.Sprintf("does not support method %s", r.Method)
-		http.Error(w, err, http.StatusInternalServerError)
-		return
-	}
-}
-
-func (srv *Server) get(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	enc := json.NewEncoder(w)
-
-	kind := r.FormValue("kind")
-	deadline := r.FormValue("deadline")
-	var items []Item
-	var err error
-
-	switch kind {
-	case "active":
-		if deadline == "today" {
-			items, err = srv.td.GetDeadline(DeadlineToday)
-		} else {
-			items, err = srv.td.GetActive()
-		}
-	case "complete":
-		items, err = srv.td.GetComplete()
-	}
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	var resp Response
-	for _, item := range items {
-		resp.Items = append(resp.Items, item)
-	}
-	if err := enc.Encode(&resp); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func (srv *Server) put(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	kind := r.FormValue("kind")
-
-	switch kind {
-	case "add":
-		var reqs []AddRequest
-		dec := json.NewDecoder(r.Body)
-		if err := dec.Decode(&reqs); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		for _, req := range reqs {
-			//var t time.Time
-			if err := srv.td.Add(req.Title, req.Detail, req.Deadline); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		}
-
-	case "update":
-		var reqs []UpdateRequest
-		dec := json.NewDecoder(r.Body)
-		if err := dec.Decode(&reqs); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		for _, req := range reqs {
-			if err := srv.td.ChangeStatus(req.ID, req.Status); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		}
-	default:
-		err := fmt.Sprintf("does not support param %s", kind)
-		http.Error(w, err, http.StatusInternalServerError)
-	}
-}
-
-func (srv *Server) delete(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	var ids []int
-	dec := json.NewDecoder(r.Body)
-	if err := dec.Decode(&ids); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	for _, id := range ids {
-		if err := srv.td.Delete(id); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	}
 }
 
 // listHandler writes list to http.ResponseWriter
